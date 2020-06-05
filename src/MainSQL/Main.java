d209aeceaf5ccfb5b48e7d3efd93af4bafacbd70
Robert Masiuk <robert@masiuk.pl>
2020-06-05 21:43:22 +0200

    second

--- src/MainSQL/Main.java
+++ src/MainSQL/Main.java
@@ -1,15 +1,31 @@
 package MainSQL;
 import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Main {
 
     public static void main(String[] args) {
+try{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = null;
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/quiz","root", "");
+        System.out.print("Database is connected !");
+        conn.close();
+    }
+    catch(Exception e) {
+        System.out.print("Do not connect to DB - Error:" + e);
+    }
+/*
+
         try {
-            String url = "http://localhost/quiz/";
+            String url = "jdbc:mysql://localhost/quiz";
             Connection conn = DriverManager.getConnection(url,"root","");
             Statement st = conn.createStatement();
-            st.executeUpdate("INSERT INTO quiz " +
-                    "VALUES (1001, '2', '2.', '3', 3)");
+            st.executeUpdate("SELECT * FROM pytania");
+                    //                   "VALUES (1001, '2', '2.', '3', 3)");
          /*   st.executeUpdate("INSERT INTO Customers " +
                     "VALUES (1002, 'McBeal', 'Ms.', 'Boston', 2004)");
             st.executeUpdate("INSERT INTO Customers " +
@@ -17,12 +33,12 @@ public class Main {
             st.executeUpdate("INSERT INTO Customers " +
                     "VALUES (1004, 'Cramden', 'Mr.', 'New York', 2001)");
 */
-            conn.close();
+  /*          conn.close();
         } catch (Exception e) {
             System.err.println("Got an exception! ");
             System.err.println(e.getMessage());
         }
-
+*/
 
     }
 }
