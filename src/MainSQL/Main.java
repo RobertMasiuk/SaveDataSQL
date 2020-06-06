54d5ad0424d49690483a62e68a7d336883567101
Robert Masiuk <robert@masiuk.pl>
2020-06-06 13:26:07 +0200

    a

--- src/MainSQL/Main.java
+++ src/MainSQL/Main.java
@@ -18,27 +18,7 @@ try{
     catch(Exception e) {
         System.out.print("Do not connect to DB - Error:" + e);
     }
-/*
 
-        try {
-            String url = "jdbc:mysql://localhost/quiz";
-            Connection conn = DriverManager.getConnection(url,"root","");
-            Statement st = conn.createStatement();
-            st.executeUpdate("SELECT * FROM pytania");
-                    //                   "VALUES (1001, '2', '2.', '3', 3)");
-         /*   st.executeUpdate("INSERT INTO Customers " +
-                    "VALUES (1002, 'McBeal', 'Ms.', 'Boston', 2004)");
-            st.executeUpdate("INSERT INTO Customers " +
-                    "VALUES (1003, 'Flinstone', 'Mr.', 'Bedrock', 2003)");
-            st.executeUpdate("INSERT INTO Customers " +
-                    "VALUES (1004, 'Cramden', 'Mr.', 'New York', 2001)");
-*/
-  /*          conn.close();
-        } catch (Exception e) {
-            System.err.println("Got an exception! ");
-            System.err.println(e.getMessage());
-        }
-*/
 
     }
 }
